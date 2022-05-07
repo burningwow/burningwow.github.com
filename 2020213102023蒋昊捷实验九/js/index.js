68f7085ae75b7c6a89421ee4ba9a2cba066e03f0
burningwow <104617077+burningwow@users.noreply.github.com>
2022-05-07 23:00:02 +0800

    Delete 2020213102023蒋昊捷实验九 directory

--- 2020213102023蒋昊捷实验九/js/index.js
+++
@@ -1,9 +0,0 @@
-var x = document.querySelectorAll("li");
-for(var i = 0;i<x.length;i++){
-  x[i].onmouseover =function() {
-    this.style.color = "red";
-  }
-  x[i].onmouseout = function() {
-    this.style.color="";
-  }
-}
